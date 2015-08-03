8834293dedf4cd4f67b93d00b5815d3be71b7bd0
Manuel <manuel.bieh.extern@zalando.de>
2015-08-03 15:50:22 +0200

    Using spaces for indentation

--- examples/examples.js
+++ examples/examples.js
@@ -1,10 +1,10 @@
 var Gini = require('../lib/gini-promise');
 
 var giniClient = new Gini({
-	clientId: 'YOUR-CLIENT-ID',
-	clientSecret: 'YOUR-CLIENT-SECRET',
-	username: 'YOUR-ACCOUNT-USERNAME',
-	password: 'YOUR-ACCOUNT-PASSWORD',
+    clientId: 'YOUR-CLIENT-ID',
+    clientSecret: 'YOUR-CLIENT-SECRET',
+    username: 'YOUR-ACCOUNT-USERNAME',
+    password: 'YOUR-ACCOUNT-PASSWORD',
 });
 
 // get all documents
@@ -14,7 +14,7 @@ giniClient.documents.get().then(function(allDocs) {
 
 // get one specific document
 giniClient.documents.get('00000000-0000-0000-0000-000000000000').then(function(doc) {
-	console.log(doc);
+    console.log(doc);
 });
 
 // search for documents
@@ -24,28 +24,28 @@ giniClient.search('test', {limit: 5}).then(function(results) {
 
 // upload new document
 gini.documents.post({
-	file: 'example.pdf'
+    file: 'example.pdf'
 }).then(function(response) {
-	return gini.request.get(response.location);
+    return gini.request.get(response.location);
 }).then(function(uploadedDoc) {
-	console.log('Uploaded document:', uploadedDoc);
+    console.log('Uploaded document:', uploadedDoc);
 }).catch(function(err) {
-	console.error('ERROR:', err);
+    console.error('ERROR:', err);
 });
 
 // usercenter request: create new user
 gini.users.post({
-	email: "test@example.org",
-	password: "supersecret"
+    email: "test@example.org",
+    password: "supersecret"
 }).then(function(response) {
-	console.log('New user can be found at', response.location);
+    console.log('New user can be found at', response.location);
 }).catch(function(err) {
-	console.log('ERROR', err);
+    console.log('ERROR', err);
 });
 
 // get one specific user
 gini.users.get('00000000-0000-0000-0000-000000000000').then(function(users) {
-	console.log('User:', user);
+    console.log('User:', user);
 }).catch(function(err) {
-	console.log('ERROR', err);
+    console.log('ERROR', err);
 });
